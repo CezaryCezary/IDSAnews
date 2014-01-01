31493abb81e3192dd632eebde3190fffa16cfe61
CezaryCezary <CezaryWronka@gmail.com>
2014-01-01 21:00:02 +0100

    Add Company class model and ICompanyRepository to use Repository pattern.

---
+++ IDSAnews.Domain/Abstract/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+﻿using IDSAnews.Domain.Entities;
+using System.Linq;
+
+namespace IDSAnews.Domain.Abstract
+{
+    public interface ICompanyRepository
+    {
+        IQueryable<Company> Companies { get; set; }
+    }
+}
---
+++ IDSAnews.Domain/Entities/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace IDSAnews.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Shortcut { get; set; }
+    }
+}
